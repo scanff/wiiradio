6a8316b1c885aaedaf90d9b2d0845828260087ca
scanff@yahoo.com <scanff@yahoo.com@21334690-463b-11de-9eaa-61f86764e705>
2009-05-22 19:30:44 +0000

    Split visuals from gui.

--- trunk/source/gui.h
+++ trunk/source/gui.h
@@ -48,7 +48,7 @@ class gui {
 
 
     fonts*          fnts;
-    fft*            vis;
+    visualizer*     vis;
 
     gui_button*     buttons[BTN_MAX];
 
@@ -64,20 +64,10 @@ class gui {
     SDL_Surface*    vol_ol;
     SDL_Surface*    dialog;
 
-
     int             genre_selected;
 
-    int             peakResults[MAX_FFT_RES];
-    int             fft_results[MAX_FFT_RES];
-
-    gui(fonts* f,fft* v) : fnts(f), vis(v), select_pos(0), row_height(30), start_y(70), genre_selected(0)
+    gui(fonts* f,visualizer* v) : fnts(f), vis(v), select_pos(0), row_height(30), start_y(70), genre_selected(0)
     {
-
-        loopi(MAX_FFT_RES) {
-            fft_results[i] = 0;
-            peakResults[i] = 0;
-        }
-
         Uint32 rmask, gmask, bmask, amask;
 //for 24bit
 
@@ -435,7 +425,8 @@ class gui {
         {
             // TO DO ... add more
             black_screen_saver();
-            if (visualize_number == 0) draw_visual();
+            if (visualize_number < MAX_VISUALS)
+                vis->draw_visuals(guibuffer,visualize_number);
 
         }else{
 
@@ -623,90 +614,6 @@ class gui {
         SDL_BlitSurface( cursor,0, guibuffer,&r);
     };
 
-    // TO DO --- put in own file/class
-    void draw_visual()
-    {
-        int x = 0;
-        int y = SCREEN_HEIGHT - 50;
-        int bar_height = 300;
-        int bar_width = SCREEN_WIDTH / MAX_FFT_RES;
-        double percent = ((double)bar_height / (double)32767);
-        int local_fft[MAX_FFT_RES];
-
-        loopi(MAX_FFT_RES) local_fft[i] = fft_results[i]; //could change as this is set via callback
-        loopi(MAX_FFT_RES)
-        {
-            // peak
-            peakResults[i] -= (32767 / bar_height) * 4;
-
-            if(peakResults[i] < 0) peakResults[i] = 0;
-            if(peakResults[i] < local_fft[i]) peakResults[i] = local_fft[i];
-
-            int peaks_newy = (y - (int)(percent * (double)peakResults[i]));
-
-            //fft
-            int newY = (y - (int)(percent * (double)local_fft[i]));
-            int h = abs(y - newY);
-
-
-
-            //draw
-            draw_rect(guibuffer,x+i*bar_width,newY,bar_width-4,h,0xcc0022 +  i*10 );
-            draw_rect(guibuffer,x+i*bar_width,peaks_newy,bar_width-4,5,0xcc0022 + i*10 << 16 );
-        }
-
-        // oscilloscope
-        int sx,sy;
-        int leftzerolevel = (SCREEN_HEIGHT/2-10)/2;
-//        int rightzerolevel = SCREEN_HEIGHT/2+10+leftzerolevel;
-        int len = (8192/4) - 1;
-        int ii;
-        double ts = static_cast<double>(SCREEN_WIDTH)/static_cast<double>(len/2);
-//        int max = 20;
- //       int min = 0;
-        double dRangePerStep = 20;
-        double timescale = leftzerolevel/dRangePerStep;
-        sx = 0;
-        sy = leftzerolevel;
-		for(ii=20; ii<len-1; ii+=1)
-		{
-			x = (int)((double)ii*ts);
-            y = (int)((double)leftzerolevel - (short)(vis->real[ii])*timescale);
-
-            bresenham_line(x,y,sx,sy,0xffffffff);
-
-            sy = y;
-            sx = x;
-		}
-
-		/*sx = 0;
-		sy = rightzerolevel;
-
-		for(ii=0; ii<len; ii++)
-		{
-			x= (int)(ii*ts);
-			y = (int)((double)rightzerolevel - (short)(vis->real[ii])*timescale);
-
-			bresenham_line(sx,sy,x,y,0xffffffff);
-            sy = y;
-            sx = x;
-		}*/
-
-		//
-
-		/*for(ii=0; ii<len-1; ii+=2)
-		{
-			x = (int)((double)ii*ts);
-            y = (int)((double)100 - (short)(vis->real[ii])*timescale);
-
-            bresenham_line(x,y,x,y-10,0xff00ffff);
-
-            sy = y;
-            sx = x;
-		}*/
-
-    }
-
     void fade(SDL_Surface *screen2, Uint32 rgb, Uint8 a)
     {
         SDL_Surface *tmp=0;
@@ -725,77 +632,6 @@ class gui {
     {
         draw_rect(guibuffer,0,0,SCREEN_WIDTH,SCREEN_HEIGHT,0);
     };
-
-
-    void bresenham_line(int x1, int y1, int x2, int y2,unsigned long color)
-    {
-        // clip
-        x1 > SCREEN_WIDTH ? x1 = SCREEN_WIDTH : x1 < 0 ? x1 = 0 : 0;
-        x2 > SCREEN_WIDTH ? x2 = SCREEN_WIDTH : x2 < 0 ? x2 = 0 : 0;
-        y1 > SCREEN_HEIGHT ? y1 = SCREEN_HEIGHT : y1 < 0 ? y1 = 0 : 0;
-        y2 > SCREEN_HEIGHT ? y2 = SCREEN_HEIGHT : y2 < 0 ? y2 = 0 : 0;
-
-
-        int delta_x = abs(x2 - x1) << 1;
-        int delta_y = abs(y2 - y1) << 1;
-
-        // if x1 == x2 or y1 == y2, then it does not matter what we set here
-        signed char ix = x2 > x1?1:-1;
-        signed char iy = y2 > y1?1:-1;
-
-        pixelColor(guibuffer,x1,y1,color);
-
-        if (delta_x >= delta_y)
-        {
-            // error may go below zero
-            int error = delta_y - (delta_x >> 1);
-
-            while (x1 != x2)
-            {
-                if (error >= 0)
-                {
-                    if (error || (ix > 0))
-                    {
-                        y1 += iy;
-                        error -= delta_x;
-                    }
-                    // else do nothing
-                }
-                // else do nothing
-
-                x1 += ix;
-                error += delta_y;
-
-                pixelColor(guibuffer,x1,y1,color);
-            }
-        }
-        else
-        {
-            // error may go below zero
-            int error = delta_x - (delta_y >> 1);
-
-            while (y1 != y2)
-            {
-                if (error >= 0)
-                {
-                    if (error || (iy > 0))
-                    {
-                        x1 += ix;
-                        error -= delta_y;
-                    }
-                    // else do nothing
-                }
-                // else do nothing
-
-                y1 += iy;
-                error += delta_x;
-
-                pixelColor(guibuffer,x1,y1,color);
-
-            }
-        }
-    }
-
 
 
 };
