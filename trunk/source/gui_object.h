0d8f48a9551b710b111a321a5b00e40cc2f4240b
knarrff <knarrff@21334690-463b-11de-9eaa-61f86764e705>
2010-02-10 21:40:32 +0000

    some more initialization

--- trunk/source/gui_object.h
+++ trunk/source/gui_object.h
@@ -57,8 +57,10 @@ class gui_object {
 
     gui_object() :
         obj_type(0),
+        s_x(0), s_y(0), s_h(0), s_w(0), text_color(0), text_color_over(0),
         bnt_down(0), bnt_out(0), bnt_over(0), bnt_on(0), bnt_off(0),
-        font_sz(0), z_order(0), bind_screen(0), center_text(true), limit_text(0)
+        font_sz(0), z_order(0), bind_screen(0), center_text(true), limit_text(0),
+        obj_state(B_OUT), pad_x(0), pad_y(0)
     {
         text_l1 = new char[SMALL_MEM + 1];
         memset(text_l1,0,SMALL_MEM + 1);
