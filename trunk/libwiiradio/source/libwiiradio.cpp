7debd00d3ff634753b1334839c7512dde3bc03d8
scanff@yahoo.com <scanff@yahoo.com@21334690-463b-11de-9eaa-61f86764e705>
2010-02-16 00:39:13 +0000

    Add libwiiradio to the project.

---
+++ trunk/libwiiradio/source/libwiiradio.cpp
@@ -0,0 +1,345 @@
+#include "globals.h"
+#include "libwiiradio.h"
+#include "client.h"
+#include "icy.h"
+
+static network* net = 0;
+static icy* icy_info = 0;
+static int connected = false;
+static int LWP_playing = false;
+static int errors = 0;
+static int mp3_volume = 255;
+#define MAX_NET_BUFFER (10000) // 10k
+
+#ifdef _WII_
+
+    static	lwp_t critical_handle = (lwp_t)NULL;
+    static void *critical_thread (void *arg);
+
+#else
+
+    static FMOD_SYSTEM             *fmod_system = 0;
+    static FMOD_SOUND              *sound1 = 0 ;
+    static FMOD_CHANNEL            *channel1 = 0;
+    static FMOD_CREATESOUNDEXINFO  exinfo;
+
+    #ifdef _WIN32
+
+        static HANDLE critical_handle = 0;
+        static unsigned critical_thread(void *arg);
+
+    #endif
+
+#endif
+
+
+static void split_url(char* url,char* _name, int* _port, char* _path)
+{
+    char url_copy[1024] = {0};
+    int part = 0;
+
+    strcpy(url_copy,url);
+
+    char* split = url_copy;
+
+     // ignore http://
+    char* search = strstr(url_copy,"http://");
+    if(!search) return;
+
+    search += strlen("http://");
+
+    split = strtok(search,":/");
+    while(split) {
+
+        if(!part) {
+            strcpy(_name,split);
+        }else if(part==1) {
+            *_port = atoi(split);
+            if (*_port == 0) {
+                strcat(_path,"/");
+                strcat(_path,split);
+            }
+        }else{
+            strcat(_path,"/");
+            strcat(_path,split);
+        }
+
+        split = strtok(0,":/");
+        part++;
+    }
+};
+
+
+static int connect(char* name)
+{
+    char url[1024] = {0};
+    char path[1024] = {0};
+    int port = 80;
+    char* host = 0;
+    char request[1024] = {0};
+
+  // -- close current connection
+    if (connected)
+    {
+        connected = 0;
+        net->client_close();
+    }
+
+    // stop mp3 if currently playing.
+    LWR_Stop();
+
+    // parse the url
+    split_url(name, url, &port, path);
+
+    // connect to new stream
+    int connect_try = net->client_connect(url,port,TCP,false);
+	if (!connect_try) connect_try = net->client_connect(url,port,TCP,false); // try again .. weird on wii
+    icy_info->icy_reset(); // reset icy state
+
+    if (connect_try) // send stream request to the server
+    {
+        host = net->get_local_ip();
+
+        //create the server request (to get the stream)
+        sprintf(request,
+                "GET %s HTTP/1.1\r\n"
+                "Host: %s\r\n"
+                "Accept: */*\r\n"
+                "User-Agent: WiiRadio-lib/1.0\r\n"
+                "Icy-Metadata: 1\r\n\r\n\r\n",
+                !(*path) ? "/" : path,host);
+
+        int len_req = strlen(request);
+        len_req = net->client_send(request,len_req);
+
+        connected = connect_try;
+
+    }else return -1;
+
+    return 1; // ok
+}
+
+
+/*
+called from libmad ....
+return the data from the buffer or a default zero stream if we are having problems keeping up!
+*/
+static s32 reader_callback(void *usr_data,void *cb,s32 len)
+{
+    return icy_info->get_buffer(cb,len);
+}
+
+// shoutcast playback
+#ifdef _WII_
+void *critical_thread(void *arg)
+#else
+unsigned __stdcall critical_thread(void *arg)
+#endif
+{
+    int len = 0;
+    char* net_buffer = 0;
+
+    net_buffer = new char[MAX_NET_BUFFER];
+    if (!net_buffer) return 0;
+
+    LWP_playing = true;
+    while(LWP_playing)
+    {
+        // stream handler
+        if (connected)
+        {
+
+            // stream handler
+            len = net->client_recv(net_buffer,MAX_NET_BUFFER);
+			
+            if(len > 0)
+            {
+                // reset errors
+                errors = 0;
+
+                // metaint handler
+                len = icy_info->parse_metaint(net_buffer,len);
+
+                // data handler
+                icy_info->buffer_data(net_buffer,len);
+
+
+            }else if(len < 0){
+
+                /* would block, not really an error in this case */
+                if (len != -11) errors++;
+
+                if (errors > 100)
+                {   // -- too many errors let's reset
+                    LWP_playing = false;
+                    break;
+                }
+            }
+
+
+            if (!icy_info->bufferring) // only play if we've buffered enough data
+            {
+			
+#ifdef _WII_
+                if(!MP3Player_IsPlaying())
+                {
+                    //modified function
+                    if(connected)
+                    {
+#ifdef STD_MAD
+                        MP3Player_PlayFile(icy_info->buffer, reader_callback, 0);
+#else
+                        MP3Player_PlayFile(icy_info->buffer, reader_callback, 0);
+#endif
+                    }
+                }
+#else
+
+                FMOD_BOOL is_playing = false;
+                FMOD_Channel_IsPlaying(channel1,&is_playing);
+                if (!is_playing)
+                {
+                    FMOD_System_CreateSound(fmod_system,(const char *)icy_info->buffer,FMOD_SOFTWARE | FMOD_OPENMEMORY_POINT | FMOD_CREATESTREAM | FMOD_CREATECOMPRESSEDSAMPLE,&exinfo, &sound1);
+                    FMOD_Sound_SetMode(sound1, FMOD_LOOP_OFF);
+                    FMOD_System_PlaySound(fmod_system,FMOD_CHANNEL_FREE, sound1, false, &channel1);
+                }
+#endif
+            }
+
+        }else{
+
+            errors = 0; // no rec errors
+            LWP_playing = false; // not connected
+        }
+
+
+        Sleep(15); // minimum delay
+    }
+
+    delete [] net_buffer; net_buffer =0;
+    if (connected) net->client_close();
+
+#ifdef _WIN32
+    _endthreadex( 0 );
+#endif
+
+    return 0;
+}
+
+// lib functions
+int LWR_Play(char* name)
+{
+    if (!name) return -1;
+
+    net = new network;
+    icy_info = new icy;
+
+    if (!connect(name)) return -2;
+
+    // set up sound system
+#ifdef _WII_
+    ASND_Init();
+    MP3Player_Init();
+    SND_ChangeVolumeVoice(0,mp3_volume,mp3_volume);
+#else
+    memset(&exinfo, 0, sizeof(FMOD_CREATESOUNDEXINFO));
+    exinfo.cbsize = sizeof(FMOD_CREATESOUNDEXINFO);
+    exinfo.length = icy_info->buffer_size;
+
+    FMOD_System_Create(&fmod_system);
+    FMOD_System_Init(fmod_system, 32, FMOD_INIT_NORMAL, NULL);
+
+#endif
+
+#ifdef _WII_
+    critical_handle = 0;
+	LWP_CreateThread(&critical_handle,critical_thread,NULL, NULL, 50000, 100);
+    if (!critical_handle) return -3;
+#else
+    #ifdef _WIN32
+    critical_handle = 0;
+    critical_handle = (HANDLE)_beginthreadex( 0, 0, &critical_thread, NULL, 0, 0 );
+    if (!critical_handle) return -3;
+    #endif
+#endif
+
+
+    return 1;
+};
+
+int LWR_Stop()
+{
+	
+	
+    if (LWP_playing)
+    {
+
+
+    LWP_playing = false;
+
+#ifdef _WII_
+	void *v;
+    LWP_JoinThread(critical_handle,&v);
+    critical_handle = 0;
+#else
+
+#ifdef _WIN32
+    WaitForSingleObject( critical_handle, INFINITE );
+    CloseHandle( critical_handle );
+    critical_handle = 0;
+#endif
+
+#endif
+
+
+#ifdef _WII_
+    if(MP3Player_IsPlaying()) MP3Player_Stop();
+#else
+    FMOD_BOOL is_playing = false;
+    FMOD_Channel_IsPlaying(channel1,&is_playing);
+    if (is_playing) FMOD_Channel_Stop(channel1);
+
+#endif
+
+    delete icy_info;
+    icy_info = 0;
+
+    delete net;
+    net = 0;
+
+#ifndef _WII_
+    FMOD_Sound_Release(sound1);
+    FMOD_System_Close(fmod_system);
+    FMOD_System_Release(fmod_system);
+#endif
+
+    } // playing
+
+    return 1;
+};
+
+// return the current URL
+char* LWR_GetUrl()
+{
+    if(!LWP_playing) return 0;
+
+    return 0;
+};
+
+// return what's playing
+char* LWR_GetCurrentTrack()
+{
+    if(!LWP_playing) return 0;
+
+    return icy_info->track_title;
+
+};
+
+// return bitrate
+int LWR_GetCurrentBitRate()
+{
+    if(!LWP_playing) return -1;
+
+    return icy_info->icy_br;
+
+};
---
+++ trunk/libwiiradio/wiitests/source/main.cpp
@@ -0,0 +1,106 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ogc/lwp_watchdog.h>
+#include <gccore.h>
+#include <wiiuse/wpad.h>
+#include "../include/libwiiradio.h"
+
+static void *xfb = NULL;
+static GXRModeObj *rmode = NULL;
+
+
+char* tests[] = {   "http://205.188.215.225:8002",
+                    "http://scfire-dtc-aa07.stream.aol.com:80/stream/1040",
+                    "http://208.76.152.74:8000"
+				};
+
+
+
+int test_lib()
+{
+    printf("\n\n\n\nTest libWiiRadio, will test three streams!\n");
+
+    for(int i = 0; i < 3;i++)
+    {
+        printf("Connecting to %s\n", tests[i]);
+
+        if(LWR_Play(tests[i]) < 0)
+        {
+            printf("ERROR!\n");
+            LWR_Stop();
+			continue;
+        }
+
+        usleep(2000000); // wait till the info is grabbed
+
+        char *title = LWR_GetCurrentTrack();
+        if (title) printf("current track = %s\n", title);
+
+        int br = LWR_GetCurrentBitRate();
+        if (br != -1) printf("current bitrate = %d\n", br);
+
+        
+		usleep(30000000); // play for a while
+		
+
+		printf("Quitting Playback!\n");
+
+        LWR_Stop();
+    }
+
+    printf("Test Done!\n");
+
+    return 0;
+}
+
+
+//---------------------------------------------------------------------------------
+int main(int argc, char **argv) {
+//---------------------------------------------------------------------------------
+
+	// Initialise the video system
+	VIDEO_Init();
+	
+
+	// Obtain the preferred video mode from the system
+	// This will correspond to the settings in the Wii menu
+	rmode = VIDEO_GetPreferredMode(NULL);
+
+	// Allocate memory for the display in the uncached region
+	xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
+	
+	// Initialise the console, required for printf
+	console_init(xfb,20,20,rmode->fbWidth,rmode->xfbHeight,rmode->fbWidth*VI_DISPLAY_PIX_SZ);
+	
+	// Set up the video registers with the chosen mode
+	VIDEO_Configure(rmode);
+	
+	// Tell the video hardware where our display memory is
+	VIDEO_SetNextFramebuffer(xfb);
+	
+	// Make the display visible
+	VIDEO_SetBlack(FALSE);
+
+	// Flush the video register changes to the hardware
+	VIDEO_Flush();
+
+	// Wait for Video setup to complete
+	VIDEO_WaitVSync();
+	if(rmode->viTVMode&VI_NON_INTERLACE) VIDEO_WaitVSync();
+
+
+	// The console understands VT terminal escape codes
+	// This positions the cursor on row 2, column 0
+	// we can use variables for this with format codes too
+	// e.g. printf ("\x1b[%d;%dH", row, column );
+	printf("\x1b[2;0H");
+	
+
+	test_lib();
+	
+
+	return 0;
+}
+
