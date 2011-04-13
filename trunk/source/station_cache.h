0a30681905e366c734146eb8c60eb5201af0020c
scanff@yahoo.com <scanff@yahoo.com@21334690-463b-11de-9eaa-61f86764e705>
2011-04-13 07:15:27 +0000

    del main trunk

--- trunk/source/station_cache.h
+++
@@ -1,9 +0,0 @@
-#ifndef STATION_CACHE_H_INCLUDED
-#define STATION_CACHE_H_INCLUDED
-
-
-extern void save_cache(char*,long,char*);
-extern bool load_cache(char*,char*);
-
-
-#endif // STATION_CACHE_H_INCLUDED
